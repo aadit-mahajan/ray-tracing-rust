87645e44fb7a1f2ff5beefd52762d62676758f1f
Aadit Mahajan <bs21b001@smail.iitm.ac.in>
2025-05-27 15:14:49 +0530

    ray and vec3 impl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+mod color;
+mod vec3;
+
+use std::io;
+use color::Color;
+use vec3::Vec3;
+
+
+
 fn main() {
-    println!("Hello, world!");
+    
+    //image
+    const IMAGE_WIDTH:u16=256;
+    const IMAGE_HEIGHT:u16=256;
+
+    print!("P3\n{} {}\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
+
+    for j in (0..IMAGE_HEIGHT).rev() {
+        eprint!("lines remaining: {}\n", j);
+        for i in 0..IMAGE_WIDTH {
+            let r = i as f32 / (IMAGE_WIDTH - 1) as f32;
+            let g = j as f32 / (IMAGE_HEIGHT - 1) as f32;
+            let b = 0.25;
+
+            let pixel_color = Color::new(r, g, b);
+            color::write_color(&mut io::stdout(), pixel_color);
+
+            // let ir = (255.999 * r) as i32;
+            // let ig = (255.999 * g) as i32;
+            // let ib = (255.999 * b) as i32;
+            // print!("{} {} {}\n", ir, ig, ib);
+        }
+    }
+    eprint!("\nDone\n");
 }
